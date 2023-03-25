0ed017e3f7675f1d90da9a91bb7cda85991faf38
Matheus RodrigueZ <102238599+Matheus019Rodrigues@users.noreply.github.com>
2023-03-24 21:05:22 -0300

    Add files via upload

---
+++ Doc.main/index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Doc_Relatorio-PDF</title>
+</head>
+
+<body>
+
+    <table>
+        <tr>
+            <td>Nome</td>
+            <td>Curso</td>
+            <td>Disciplina</td>
+            <td>Media</td>
+        </tr>
+    </table>
+
+
+    <table>
+        <tr>
+            <td>Gledson</td>
+            <td>ADS</td>
+            <td>Liguagem tecnica de Programação 2</td>
+            <td>8.5</td>
+        </tr>
+    </table>
+
+
+    <table>
+        <tr>
+            <td>Kennet</td>
+            <td>ADS</td>
+            <td>Liguagem tecnica de Programação 2</td>
+            <td>7.0</td>
+        </tr>
+    </table>
+
+
+    <table>
+        <tr>
+            <td>João Victor</td>
+            <td>ADS</td>
+            <td>Liguagem tecnica de Programação 2</td>
+            <td>9.0</td>
+        </tr>
+    </table>
+
+
+    <table>
+        <tr>
+            <td>Marcus</td>
+            <td>ADS</td>
+            <td>Liguagem tecnica de Programação 2</td>
+            <td>7.5</td>
+        </tr>
+    </table>
+
+
+    <table>
+        <tr>
+            <td>Lucas</td>
+            <td>ADS</td>
+            <td>Liguagem tecnica de Programação 2</td>
+            <td>6.8</td>
+        </tr>
+    </table>
+
+
+    <table>
+        <tr>
+            <td>Gabriel</td>
+            <td>ADS</td>
+            <td>Liguagem tecnica de Programação 2</td>
+            <td>7.7</td>
+        </tr>
+    </table>
+
+                    <a href="instrut.php" >Formatação em pdf</a>
+
+</body>
+
+</html>
